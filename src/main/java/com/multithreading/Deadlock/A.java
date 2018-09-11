5299230d3222548fdfcd869f9c416016c52ad101
saurabh <saurabh945644@gmail.com>
2018-09-11 19:30:09 +0530

    Added Deadlocks

---
+++ src/main/java/com/multithreading/Deadlock/A.java
@@ -0,0 +1,28 @@
+package com.multithreading.Deadlock;
+
+public class A {
+
+    private Object key1 = new Object();
+    private Object key2 = new Object();
+
+    public void a() {
+        synchronized (key1) {
+            System.out.println("["+Thread.currentThread().getName()+"] I am in a()");
+            b();
+        }
+    }
+
+    public void b() {
+        synchronized (key2) {
+            System.out.println("["+Thread.currentThread().getName()+"] I am in b()");
+            c();
+        }
+    }
+
+    public void c(){
+        synchronized (key1) {
+            System.out.println("["+Thread.currentThread().getName()+"] I am in c()");
+            a();
+        }
+    }
+}
---
+++ src/main/java/com/multithreading/ProducerConsumer/raceConndition/ProducerConsumer.java
@@ -0,0 +1,67 @@
+package com.multithreading.ProducerConsumer.raceConndition;
+
+public class ProducerConsumer {
+
+    private static int[] buffer;
+    private static int count;
+
+    static class Producer {
+
+        private void produce() {
+            while (isFull(buffer)) {}
+            buffer[count++] = 1;
+        }
+
+    }
+
+    static class Consumer {
+
+        void consume() {
+            while(isEmpty(buffer)) {}
+                buffer[--count] = 0;
+
+        }
+    }
+
+    static boolean isEmpty(int[] buffer) {
+        return count == 0;
+    }
+
+    static boolean isFull(int[] buffer) {
+        return count == buffer.length;
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        buffer = new int[10];
+        count = 0;
+
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        Runnable produceTask = () -> {
+            for(int i = 0; i < 50; i++) {
+                producer.produce();
+            }
+            System.out.println("Done Producing");
+        };
+
+        Runnable consumeTask = () -> {
+            for(int i = 0; i < 50; i++) {
+                consumer.consume();
+            }
+            System.out.println("Done Consuming");
+        };
+
+        Thread consumerThread = new Thread(consumeTask);
+        Thread producerThread = new Thread(produceTask);
+
+        consumerThread.start();
+        producerThread.start();
+
+        consumerThread.join();
+        producerThread.join();
+
+        System.out.println("Data in the Buffer " + count);
+    }
+
+}
