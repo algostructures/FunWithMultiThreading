28a5764ec3c032dcd41ffb24432e4f70885d6491
saurabh <saurabh945644@gmail.com>
2018-09-25 21:31:00 +0530

    Replacing With lambda

--- src/main/java/com/multithreading/ThreadPool/App.java
+++ src/main/java/com/multithreading/ThreadPool/App.java
@@ -4,6 +4,7 @@ package com.multithreading.ThreadPool;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 class Processor implements Runnable {
 
@@ -31,9 +32,8 @@ public class App {
     public static void main(String args[]) {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        for (int i = 0; i<5; i++) {
-            executorService.submit(new Processor(i));
-        }
+        IntStream.range(0,5).forEach(i-> executorService.submit(new Processor(i)));
+
 
         executorService.shutdown();
 
