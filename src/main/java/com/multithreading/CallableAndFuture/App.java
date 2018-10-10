bd4fc2568c9e8283ebfb5e27adbb12941c170477
saurabh <saurabh945644@gmail.com>
2018-10-10 18:06:43 +0530

    collapsed catch block

--- src/main/java/com/multithreading/CallableAndFuture/App.java
+++ src/main/java/com/multithreading/CallableAndFuture/App.java
@@ -33,9 +33,7 @@ public class App {
 
         try {
             System.out.println("Result is "+ future.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
     }
